3791180cad8b88dfad2d593ce20067aaa083adeb
Tommy Pang <tommypang04@gmail.com>
2024-05-25 23:17:53 -0400

    updated comments and prediction flow

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from data.dataset import read_dataset
-from db.util import create_table, register, login, get_available_tasks, get_assigned_tasks_miner, assign_task
+from db.util import *
 
 app = Flask(__name__)
 
@@ -11,30 +11,45 @@ create_table()
 @app.route("/signup", methods=["POST"])
 def signup():
     data = request.get_json()
+    print(data)
     username = data['username']
     password = data['password']
     validator = data['validator']
-    return register(username, password, validator)
+    return jsonify({"response": register(username, password, validator)}), 201
 
 @app.route("/login", methods=["POST"])
 def authenticate():
     data = request.get_json()
     username = data['username']
     password = data['password']
-    return login(username, password)
+    status = login(username, password)
+    return jsonify({"response": "Login successful"}), 201 if status == "Login successful" else jsonify({"response" : "Invalid username or password"}), 401
 
 @app.route("/get_available_tasks", methods=["GET"])
 def get_tasks_for_validator():
-    return get_available_tasks()
+    return jsonify({"response": get_available_tasks()}), 200
 
 @app.route("/get_assigned_tasks/<user_id>", methods=["GET"])
 def get_tasks_for_miner(user_id):
-    return get_assigned_tasks_miner(user_id)
+    return jsonify({"response": get_assigned_tasks_miner(user_id)}), 200
 
 @app.route("/assign_task", methods=["POST"])
 def add_task():
     task = request.get_json()
-    return assign_task(task)
+    return jsonify({"response": assign_task(task)}), 200
+
+@app.route("/predict", methods=["POST"])
+def make_prediction():
+    prediction = request.get_json()
+    return jsonify({"response": insert_prediction(prediction)}), 200
+
+@app.route("/get_posted_tasks/<validator_id>", methods=["GET"])
+def get_posted_tasks(validator_id):
+    return jsonify({"response": get_all_tasks(validator_id)}), 200
+
+@app.route("/get_predictions/<task_id>", methods=["GET"])
+def get_predictions(task_id):
+    return jsonify({"response": get_all_predictions(task_id)}), 200
 
 if __name__ == "__main__":
     app.run()
--- db/util.py
+++ db/util.py
@@ -1,6 +1,8 @@
 import MySQLdb
 from flask import jsonify
 from data.dataset import read_dataset, datasets
+import random
+from datetime import datetime
 
 # MySQL configurations
 MYSQL_HOST = '127.0.0.1'
@@ -18,6 +20,7 @@ def get_db_connection():
         db=MYSQL_DB
     )
 
+# run to init tables in local db
 def create_table():
     db = get_db_connection()
     cursor = db.cursor()
@@ -33,13 +36,17 @@ def create_table():
     """)
     
     # Create predictions table
+    # batch prediction = {1: 1 day, 2: 1 week, 3: 1 month}
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS predictions (
             id BIGINT AUTO_INCREMENT PRIMARY KEY,
-            username VARCHAR(255),
-            result TEXT,
-            time_span BIGINT,
-            task_id BIGINT
+            result DOUBLE,
+            miner_id BIGINT,
+            validator_id BIGINT,
+            task_id BIGINT,
+            batch INT,
+            time_stamp DATE,
+            loss DOUBLE
         )
     """)
 
@@ -59,17 +66,21 @@ def create_table():
     cursor.close()
     db.close()
 
-def register(username: str, password: str, validator: bool):
-    validator_bit = 1 if validator else 0
+# register user with username password, and validator to indicate if user is a validator
+def register(username, password, validator):
+    validator_bit = 0
+    if validator == 'true':
+        validator_bit = 1
     db = get_db_connection()
     cursor = db.cursor()
     cursor.execute("INSERT INTO users (username, password, validator) VALUES (%s, %s, %s)", (username, password, validator_bit))
     db.commit()
     cursor.close()
     db.close()
-    return jsonify({"response": "User registered successfully"}), 201
+    return "User registered successfully"
 
-def login(username: str, password: str):
+# login with credential
+def login(username, password):
     db = get_db_connection()
     cursor = db.cursor()
     cursor.execute("SELECT password FROM users WHERE username = %s", [username])
@@ -77,10 +88,31 @@ def login(username: str, password: str):
     cursor.close()
     db.close()
     if user and user[0] == password:
-        return jsonify({"response": "Login successful"}), 200
+        return "Login successful"
     else:
-        return jsonify({"response" : "Invalid username or password"}), 401
-    
+        return "Invalid username or password"
+
+# given a user id for miner, fetch all tasks assigned to them in the format:
+"""
+tasks = [
+{
+    'task_id': task_id,
+    'miner_id': miner_id,
+    'validator_id': validator_id,
+    'task_name': task_name,
+    'task_description': task_description,
+    'dataset': raw_dataset
+},
+{
+    'task_id': task_id2,
+    'miner_id': miner_id2,
+    'validator_id': validator_id2,
+    'task_name': task_name2,
+    'task_description': task_description2,
+    'dataset': raw_dataset2
+}
+]
+"""
 def get_assigned_tasks_miner(user_id):
     db = get_db_connection()
     cursor = db.cursor()
@@ -100,11 +132,23 @@ def get_assigned_tasks_miner(user_id):
         formatted_tasks.append(formatted_task)
     cursor.close()
     db.close()
-    return jsonify({"response": formatted_task}), 200
+    return formatted_tasks
 
+# get all potential tasks that can be used
 def get_available_tasks():
-    return jsonify({"response": datasets}), 200
+    return datasets
 
+# assign a task to a miner, the task is in the format example:
+"""
+{
+    "miner_id": 2,
+    "validator_id": 1,
+    "task_name": "Employment Level - Bachelor's Degree and Higher, 25 Yrs. & over",
+    "task_description": "Predict Employment Level for Bachelor's Degree and Higher, 25 Yrs. & over for the next week/month/year",
+    "dataset_id": "LNS12027662"
+}
+"""
+# note the dataset_id should match one of the task_ids in dataset.py
 def assign_task(task):
     miner_id = task['miner_id']
     validator_id = task['validator_id']
@@ -120,4 +164,113 @@ def assign_task(task):
     db.commit()
     cursor.close()
     db.close()
-    return jsonify({"response": "task is assigned"}), 200
+    return "Task is assigned"
+
+# insert a prediction into the database, prediction is in the format:
+"""
+{
+    "result": "0.1134",
+    "miner_id": 2,
+    "validator_id": 1,
+    "task_id": 1,
+    "batch": 1
+}
+"""
+def insert_prediction(prediction):
+    result = prediction['result']
+    miner_id = prediction['miner_id']
+    validator_id = prediction['validator_id']
+    task_id = prediction['task_id']
+    batch = prediction['batch']
+
+    db = get_db_connection()
+    cursor = db.cursor()
+    cursor.execute('''
+                INSERT INTO predictions (result, miner_id, validator_id, task_id, batch, time_stamp, loss)
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+            ''', (result, miner_id, validator_id, task_id, batch, datetime.today().strftime('%Y-%m-%d'), random.random()))
+    db.commit()
+    cursor.close()
+    db.close()
+    return "Prediction is inserted"
+
+
+# get all tasks posted by the validator using validator_id, result is of the format:
+"""
+[
+{
+    'miner_id': miner_id,
+    'validator_id': validator_id,
+    'task_name': task_name,
+    'task_description': task_description
+},
+{
+    'miner_id': miner_id2,
+    'validator_id': validator_id2,
+    'task_name': task_name2,
+    'task_description': task_description2
+}
+]
+"""
+def get_all_tasks(validator_id):
+    db = get_db_connection()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM tasks WHERE validator_id = %s", [validator_id])
+    tasks = cursor.fetchall()
+    formatted_tasks = []
+    for task in tasks:
+        _, miner_id, validator_id, task_name, task_description, _, = task
+        formatted_task = {
+            'miner_id': miner_id,
+            'validator_id': validator_id,
+            'task_name': task_name,
+            'task_description': task_description
+        }
+        formatted_tasks.append(formatted_task)
+    cursor.close()
+    db.close()
+    return formatted_tasks
+
+
+# get all predictions made by the miners for a specific task with task_id, the result is like:
+"""
+[{
+    'result': result,
+    'miner_id': miner_id,
+    'validator_id': validator_id,
+    'task_id': task_id,
+    'batch': batch,
+    'time_stamp': time_stamp,
+    'loss': loss
+},
+{
+    'result': result2,
+    'miner_id': miner_id2,
+    'validator_id': validator_id2,
+    'task_id': task_id2,
+    'batch': batch2,
+    'time_stamp': time_stamp2,
+    'loss': loss2
+}]
+"""
+def get_all_predictions(task_id):
+    db = get_db_connection()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM predictions WHERE task_id = %s", [task_id])
+    predictions = cursor.fetchall()
+    formated_predictions = []
+    for prediction in predictions:
+        _, result, miner_id, validator_id, task_id, batch, time_stamp, loss = prediction
+        formatted_task = {
+            'result': result,
+            'miner_id': miner_id,
+            'validator_id': validator_id,
+            'task_id': task_id,
+            'batch': batch,
+            'time_stamp': time_stamp,
+            'loss': loss
+        }
+        formated_predictions.append(formatted_task)
+    cursor.close()
+    db.close()
+    return formated_predictions
